14533ffbfed923feb700fff8d86e2a555a00f184
Serge Bogachev <serge2day@gmail.com>
2016-03-18 11:38:47 +0100

    Initial Commit

---
+++ Pockedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pockedex
+//
+//  Created by Serge Bogachev on 18.03.16.
+//  Copyright © 2016 Theros AG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
