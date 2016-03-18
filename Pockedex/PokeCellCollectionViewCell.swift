244fed04d23ec81487becfe12f87389de92ff681
Serge Bogachev <serge2day@gmail.com>
2016-03-18 13:25:15 +0100

    Did the Initial Layout Added a PokeCellCollectionViewCell View

---
+++ Pockedex/PokeCellCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCellCollectionViewCell.swift
+//  Pockedex
+//
+//  Created by Serge Bogachev on 18.03.16.
+//  Copyright © 2016 Theros AG. All rights reserved.
+//
+
+import UIKit
+
+class PokeCellCollectionViewCell: UICollectionViewCell {
+    
+}
