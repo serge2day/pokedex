cb1445bc6f7259011bf774ce4e459ccc2f4c7ebd
Serge Bogachev <serge2day@gmail.com>
2016-03-18 13:56:46 +0100

    Connected the Layout Collection to the VeiwContoller

--- Pockedex/PokeCellCollectionViewCell.swift
+++ Pockedex/PokeCellCollectionViewCell.swift
@@ -10,4 +10,28 @@ import UIKit
 
 class PokeCellCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        self.layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+    
+    
 }
--- Pockedex/ViewController.swift
+++ Pockedex/ViewController.swift
@@ -8,13 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    
+    @IBOutlet weak var collection: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        collection.delegate = self
+        collection.dataSource = self
         
         
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PokeCell", forIndexPath: indexPath) as? PokeCellCollectionViewCell {
+            
+            let pokemon = Pokemon(name: "test", pokedexId: indexPath.row)
+            cell.configureCell(pokemon)
+            
+            return cell
+        
+        } else {
+            return UICollectionViewCell()
+        }
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView, didDeselectItemAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSizeMake(105, 105)
     }
 
 
