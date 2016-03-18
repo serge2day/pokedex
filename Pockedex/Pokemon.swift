7e31dacac77b7404772ff48ab30d745824dfda23
Serge Bogachev <serge2day@gmail.com>
2016-03-18 12:27:28 +0100

    Created a Pokemon Class

---
+++ Pockedex/Pokemon.swift
@@ -0,0 +1,28 @@
+//
+//  Pokemon.swift
+//  Pockedex
+//
+//  Created by Serge Bogachev on 18.03.16.
+//  Copyright © 2016 Theros AG. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name:String, pokedexId:Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+}
